f716260db2bf026fd34c0b4499117cad46d7f14d
floriankark <florian.kark@gmail.com>
2025-01-11 15:56:42 +0100

    refactor: :art: refactor data paths by combining fom and transmittance
    Note: code in the notebooks need to be refactored to the new path; use dataloader once available

--- config/path.py
+++ config/path.py
@@ -4,5 +4,4 @@ import os
 project_root = (
     Path(os.path.realpath(__file__)).parent.parent / "transmittance2fom_genmodels"
 )
-VERVET_TRANS_DATA = project_root / "data" / "transmittance"
-VERVET_FOM_DATA = project_root / "data" / "fom"
+VERVET_DATA = project_root / "data"
