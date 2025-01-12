912bc4c354b274d71c3f9f22b7028e7072efbe50
Alvi, Mashhood (moy75pic) <mashhood.alvi@hhu.de>
2025-01-13 00:47:47 +0100

    fixed data loader und test_loader

--- transmittance2fom_genmodels/notebooks/dataset.py
+++ transmittance2fom_genmodels/notebooks/dataset.py
@@ -1,3 +1,4 @@
+import os
 import torch
 import h5py
 import random
@@ -5,6 +6,7 @@ import glob
 from collections import namedtuple
 from torch.utils.data import Dataset, Sampler, DataLoader
 from config.path import VERVET_DATA
+import numpy as np
 
 Tile = namedtuple('Tile', 'brain, section, region, map_type, row, column, patch_size')
 resolution_level = '05' # TODO: Make this part of a config file
@@ -29,7 +31,8 @@ class HDF5Dataset(Dataset):
         self._open_hdf5(loc)
         image = self._hf["pyramid"][resolution_level]
         patch = image[loc.row:loc.row + loc.patch_size, loc.column:loc.column + loc.patch_size]
-        
+        if patch.shape[:2] != (loc.patch_size, loc.patch_size):
+            patch = pad_patch(patch, loc.patch_size)  
         if loc.map_type == "FOM":
             patch = patch.transpose(2, 0, 1)
         else:
@@ -63,10 +66,20 @@ class HDF5Sampler(Sampler):
         for _ in range(self.tiles_per_epoch):
             files = glob.glob(f"{VERVET_DATA}/{self.brain}*{self.map_type}*.h5")
             file_path = random.choice(files)
-            section, region = file_path.split("_")[1:3]
-                    
+            #section, region = file_path.split("_")[1:3] 
+
+            file_name = os.path.basename(file_path)
+                        
+            parts = file_name.split("_")
+            section = parts[1]  # Second part is always the section
+            if parts[2] not in ["FOM", "NTransmittance"]:
+                region = parts[2]  # Region is present
+            else:
+                region = "none"   
             self._open_hdf5(file_path)
-            row_len, col_len = self._hf["pyramid"][resolution_level].shape
+            #row_len, col_len = self._hf["pyramid"][resolution_level].shape
+            shape = self._hf["pyramid"][resolution_level].shape
+            row_len, col_len = shape[:2]  # Extract the first two dimensions
             row_range = row_len - self.patch_size
             col_range = col_len - self.patch_size
             row, col = random.randint(0, row_range), random.randint(0, col_range)
@@ -89,3 +102,8 @@ def create_dataloader(brain: str,
     sampler = HDF5Sampler(brain=brain, map_type=map_type, patch_size=patch_size, tiles_per_epoch=tiles_per_epoch) 
     dataloader = DataLoader(dataset, batch_size=batch_size, sampler=sampler, num_workers=num_workers)
     return dataloader
+
+def pad_patch(patch, target_size):
+    pad_height = max(0, target_size - patch.shape[0])
+    pad_width = max(0, target_size - patch.shape[1])
+    return np.pad(patch, ((0, pad_height), (0, pad_width), (0, 0)), mode='constant')
